9ba581acc66cd730d3f565c885ca95b342a5832f
Min <minsang1019@gmail.com>
2026-07-23 22:25:31 +0900

    3주차 과제 제출

--- week03/MINSANG/5105.py
+++ week03/MINSANG/5105.py
@@ -43,16 +43,15 @@ for tc in range(1, T + 1):
             # 조건 1: 미로 안이고, 아직 안 가본 칸인가?
             if 0 <= nr < N and 0 <= nc < N and not visited[nr][nc]:
                 
-                # 상황 A: 다음 칸이 도착점(3)인 경우!
+                # 상황 A: 다음 칸이 도착점(3)인 경우
                 if maze[nr][nc] == 3:
-                    ans = dist  # 지금까지 거쳐온 0의 개수(dist)가 바로 정답!
+                    ans = dist  # 지금까지 거쳐온 0의 개수(dist)가 바로 정답
                     q.clear()   # 탐색 완료했으니 큐를 비워서 while문 탈출
                     break
 
                 # 상황 B: 다음 칸이 갈 수 있는 길(0)인 경우
                 elif maze[nr][nc] == 0:
-                    visited[nr][nc] = True        # 방문했다고 도장 쾅!
-                    q.append((nr, nc, dist + 1))  # '거리+1' 해서 다음 대기열 줄 맨 뒤에 세움!
+                    visited[nr][nc] = True        # 방문했다고 표시
+                    q.append((nr, nc, dist + 1))  # '거리+1' 해서 다음 대기열 줄 맨 뒤에 세움
 
-    # 6. 결과 출력
     print(f"#{tc} {ans}")
